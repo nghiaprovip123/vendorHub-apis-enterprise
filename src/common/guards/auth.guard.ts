d33f9422d0107151451396c2f6caa08c751b2a31
nghiaprovip123 <wrefree549@gmail.com>
2026-01-26 16:28:56 +0700

    Refactor Authentication Guard

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -1,41 +1,107 @@
 import { jwtService } from "@/common/jwt/index.jwt"
-
+import { Request, Response, NextFunction } from "express"
+import { GuardError } from "@/common/utils/error/guard.error"
+declare global { 
+    namespace Express { 
+        interface Request { 
+            user?: string | any 
+        } 
+    } 
+}
 export class AuthGuard {
-    constructor(private readonly token: string) {
-        if (!token) {
-            throw new Error("THIẾU THÔNG TIN TOKEN");
+    static RequireAuth ( req: Request, res: Response, next: NextFunction ) {
+        try {
+            const authHeader = req.headers.authorization
+
+            if (!authHeader) {
+                return res.status(401).json(
+                    {
+                        success: false,
+                        message: GuardError.MISSING_AUTHENTICATION_ERROR
+                    }
+                )
+            }
+    
+            const token = authHeader.split(" ")[1]
+    
+            if (!token) {
+                return res.status(401).json(
+                    {
+                        success: false,
+                        message: GuardError.MISSING_TOKEN_ERROR
+                    }
+                )
+            }
+    
+            const decoded = jwtService.verifyAccessToken(token)
+    
+            if (!decoded) {
+                return res.status(401).json(
+                    {
+                        success: false,
+                        message: GuardError.WRONG_ACCESS_TOKEN_INFORMATION
+                    }
+                )        
+            }
+            
+            req.user = decoded
+            next()
         }
+        catch (error: any) {
+            if (error.name === 'TokenExpiredError') {
+                return res.status(401).json({
+                    success: false,
+                    message: GuardError.TOKEN_IS_EXPIRED
+                });
+            }
+            
+            return res.status(401).json({
+                success: false,
+                message: 'FAIL TO AUTHENTICATE'
+            });
+        } 
     }
 
-    verifyToken() {
-        const decoded = jwtService.verifyAccessToken(this.token);
-        if (!decoded) {
-            throw new Error('SAI TOKEN, VUI LÒNG THỬ LẠI');
-        }
-        return decoded;
-    }
+    static RequireRefreshToken ( req: Request, res: Response, next: NextFunction ) {
+        try {
+            const refreshToken = req.cookies?.refreshToken
 
-    extractTokenPayload() {
-        const payload = this.verifyToken();
-        if (!payload) {
-            throw new Error("KHÔNG TỒN TẠI TOKEN PAYLOAD");
+            if (!refreshToken) {
+                return res.status(401).json(
+                    {
+                        success: false,
+                        message: GuardError.MISSING_REFRESH_TOKEN_ERROR
+                    }
+                )
+            }
+    
+            const decoded = jwtService.verifyRefreshToken(refreshToken)
+    
+            if (!decoded) {
+                return res.status(401).json(
+                    {
+                        success: false,
+                        message: GuardError.WRONG_REFRESH_TOKEN_INFORMATION
+                    }
+                )
+            }
+            
+            req.user = decoded
+            next()
         }
-        return payload;
-    }
+        catch (error: any) {
+            if (error.name === 'TokenExpiredError') {
+                return res.status(401).json({
+                    success: false,
+                    message: GuardError.WRONG_REFRESH_TOKEN_INFORMATION
+                });
+            }
+            
+            return res.status(401).json({
+                success: false,
+                message: 'FAIL TO AUTHENTICATE'
+            });
+        }
 
-    verifyRefreshToken() {
-        const decoded = jwtService.verifyRefreshToken(this.token);
-        if (!decoded) {
-            throw new Error('SAI TOKEN, VUI LÒNG THỬ LẠI');
-        }
-        return decoded;    
-    }
-
-    extractAccessTokenPayload() {
-        const payload = this.verifyRefreshToken();
-        if (!payload) {
-            throw new Error("KHÔNG TỒN TẠI TOKEN PAYLOAD");
-        }
-        return payload;
     }
 }
