8969956b763d0209b3aac057cd005570371e0060
nghiaprovip123 <wrefree549@gmail.com>
2026-02-24 15:03:30 +0700

    Rework the API

--- src/service/resolvers/view-all-service.resolver.ts
+++ src/service/resolvers/view-all-service.resolver.ts
@@ -1,11 +1,20 @@
 import { prisma } from "@/lib/prisma"
+import { CategoryLevel } from "@prisma/client"
 
 const viewAllService = async (
     _: unknown,
     args : { input : any },
     ctx: any
 ) => {
-    const allService = await prisma.service.findMany()
+    const allService = await prisma.service.findMany(
+        {
+            where : {
+                category : {
+                    level : CategoryLevel.LEVEL_1
+                }
+            }
+        }
+    )
 
     return {
         items : allService
