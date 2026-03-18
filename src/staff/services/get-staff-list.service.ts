4501833635dafe05bf5d1f7ab7a2c54c398b03ac
nghiaprovip123 <wrefree549@gmail.com>
2026-03-18 09:44:45 +0700

    Redis Onboarding

--- src/staff/services/get-staff-list.service.ts
+++ src/staff/services/get-staff-list.service.ts
@@ -3,27 +3,46 @@ import { StaffRepository } from "@/staff/repositories/staff.repository"
 import { StaffError } from "@/common/utils/error/staff.error"
 import ApiError from "@/common/utils/ApiError.utils"
 import { DEFAULT_PAGE_SIZE } from "@/common/utils/constraint/pagnition"
+import  redisClient  from "@/lib/redis"
 
-export const getStaffListService = async ( page: number) => {
+export const getStaffListService = async (page: number) => {
   try {
+
+    const currentPage = page ?? 1
+    const cacheKey = `staff:list:page:${currentPage}`
+
+    const cached = await redisClient.get(cacheKey)
+
+    if (cached) {
+      return JSON.parse(cached)
+    }
+
     const staffRepos = new StaffRepository(prisma)
-    const getPage = page ?? 1
-    const skip = (getPage - 1) * DEFAULT_PAGE_SIZE
 
-    const staffs = await staffRepos.getPagnition(skip, DEFAULT_PAGE_SIZE)
+    const skip = (currentPage - 1) * DEFAULT_PAGE_SIZE
 
-    const total = staffRepos.count()
-    const totalActive = staffRepos.countByStatus(true)
-    const totalInActive = staffRepos.countByStatus(false)
-    const totalNewInMonth = staffRepos.countNewInMonth()
+    const [staffs, total, totalActive, totalInActive, totalNewInMonth] =
+      await Promise.all([
+        staffRepos.getPagnition(skip, DEFAULT_PAGE_SIZE),
+        staffRepos.count(),
+        staffRepos.countByStatus(true),
+        staffRepos.countByStatus(false),
+        staffRepos.countNewInMonth()
+      ])
 
-    return {
+    const result = {
       items: staffs,
       total,
       totalActive,
       totalInActive,
       totalNewInMonth
     }
+
+    await redisClient.set(cacheKey, JSON.stringify(result), { expiration: { type: "EX", value: 60 } }
+  )
+
+    return result
+
   } catch (error) {
     throw new ApiError(500, StaffError.FETCH_STAFF_LIST_ERROR)
   }
