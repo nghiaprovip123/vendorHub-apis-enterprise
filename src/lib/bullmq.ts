e0d17563c6d6494c88036baedf58e2f979511be0
nghiaprovip123 <wrefree549@gmail.com>
2026-03-20 16:25:06 +0700

    Revert "Removal"
    This reverts commit b9f1d1bb010c49b16db7da82c79ae9984aa0059e.

--- src/lib/bullmq.ts
+++ src/lib/bullmq.ts
@@ -1,6 +1,6 @@
 // infrastructure/redis/bullmq.connection.ts
 export const bullmqConnection = {
-  host: process.env.BULLMQ_HOST,
+  host: process.env.BULLMQ_HOST ?? "127.0.0.1",
   port: Number(process.env.BULLMQ_PORT) ?? 6379,
   password: process.env.BULLMQ_PASSWORD,
   maxRetriesPerRequest: null,   // bắt buộc cho BullMQ
